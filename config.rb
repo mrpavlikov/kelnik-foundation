698f21904ad503725896066e3b29e55f65431f9c
aurfon <aurfon@mail.ru>
2014-07-15 16:05:03 +0400

    Compass settings

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Set this to the root of your project when deployed:
+http_path = "www"
+css_dir = "www/css"
+sass_dir = "sass"
+#images_dir = "www/img"
+#javascripts_dir = "www/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
